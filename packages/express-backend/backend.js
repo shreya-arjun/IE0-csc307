4b7815b709940bf9950c437817619ef411c66e38
Shreya  Arjun <shreyaarjun@Shreyas-MacBook-Air.local>
2025-01-21 13:42:35 -0800

    part 6

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,8 +35,8 @@ const users = {
       }
     ]
   };
-
   
+
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
@@ -46,12 +46,29 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/users", (req, res) => {
-res.send(users);
+  const name = req.query.name;
+  const job = req.query.job;
+  let result = users["users_list"];
+  
+  if (name != undefined) {
+    result = findUserByName(name);
+  }
+
+  if (job != undefined) {
+    result = result.filter((user) => user["job"] === job);
+
+  res.send(result);
+  }
 });
 
 app.get("/users", (req, res) => {
@@ -65,6 +82,10 @@ app.get("/users", (req, res) => {
   }
 });
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
@@ -75,20 +96,22 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
 });
 
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let deletedUser = findUserById(id);
+  let updatedList = users["users_list"].filter((user) => user !== deletedUser);
+  res.send(updatedList);
+});
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
 });
+
